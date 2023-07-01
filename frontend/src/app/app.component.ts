4ed909cdebc0a830ab292fb61539f5c37177d3fb
Saleck3 <aledagonale@gmail.com>
2023-06-30 22:09:32 -0300

    Oculta boton + sin login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,11 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '20231erCuatGrupo9';
-  jwt: string|null;
+  public jwt: string|null;
 
   constructor() {
     this.jwt = localStorage.getItem("jwt");
   }
 
-
+  getJWT(){
+    return this.jwt;
+  }
 }
--- frontend/src/app/galeria/galeria.component.ts
+++ frontend/src/app/galeria/galeria.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Product } from "../carrito/product";
+import { AppComponent } from '../app.component';
 import { CarritoComponent } from '../carrito/carrito.component';
 
 import { Router } from '@angular/router';
@@ -14,13 +15,15 @@ import { share, catchError } from 'rxjs/operators';
   styleUrls: ['./galeria.component.css']
 })
 export class GaleriaComponent {
+  public jwt: string | null;
 
   products: Product[] = [];
 
-  carrito: any ;
+  carrito: any;
 
-  constructor(protected router: Router, protected httpClient: HttpClient) {
-    this.carrito  = new CarritoComponent(httpClient);
+  constructor(protected router: Router, protected httpClient: HttpClient, app: AppComponent) {
+    this.jwt = app.jwt;
+    this.carrito = new CarritoComponent(httpClient);
     let res: Observable<Product[]> =
       this.httpClient.get<Product[]>('http://localhost:3000/galeria');
 
